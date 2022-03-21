d647dcae001d1a7d645292cfb6d843cb8435c9c7
권지현 <dial2825@naver.com>
2022-03-21 23:28:18 +0900

    BFS, DFS 실전문제

---
+++ 이것이 코딩테스트다 with Python/Part02/DFS BFS/02.py
@@ -0,0 +1,31 @@
+# 미로 탈출
+# 풀이 해설
+from collections import deque
+
+n, m = map(int, input().split())
+
+arr = []
+
+for i in range(n):
+    arr.append(list(map(int, input())))
+
+# 북, 동, 남, 서 방향 정의
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+            if arr[nx][ny] == 1:
+                arr[nx][ny] = arr[nx][ny] + 1
+                queue.append((nx, ny))
+    return arr[n - 1][m - 1]
+
+print(bfs(0, 0))
