db5ab362f2ff4ab6bfa1b3241be603746102309b
JiHyunKwon98 <dial2825@naver.com>
2022-07-13 02:17:12 +0900

    124 나라의 숫자

---
+++ 프로그래머스/level2/09.py
@@ -0,0 +1,17 @@
+# 124 나라의 숫자
+
+def solution(n):
+    num = ['1', '2', '4']
+
+    answer = ""
+
+    while n > 0:
+        n -= 1
+
+        answer = num[n % 3] + answer
+
+        n //= 3
+
+    return answer
+
+print(solution(5))
